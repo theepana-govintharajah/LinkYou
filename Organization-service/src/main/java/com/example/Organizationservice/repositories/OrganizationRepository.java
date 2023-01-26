820cd1c891e6381f8aedca90ecac0e1bf8285804
Theepana Govintharajah <63109025+theepana-govintharajah@users.noreply.github.com>
2023-01-26 05:41:49 +0530

    Change main file structure

--- Organization-service/src/main/java/com/example/Organizationservice/repositories/OrganizationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Organizationservice.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.Organizationservice.models.Organization;
-
-public interface OrganizationRepository extends JpaRepository<Organization, Integer> {
-    Organization findByName(String name);
-}
